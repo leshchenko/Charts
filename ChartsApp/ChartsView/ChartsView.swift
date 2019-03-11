acff045576303d6f0dfdae662c883e566fe24a77
Ruslan Leshchenko <leshchenkoruua@gmail.com>
2019-03-11 20:41:39 +0200

    Draw charts with their own colorts

--- ChartsApp/ChartsView/ChartsView.swift
+++ ChartsApp/ChartsView/ChartsView.swift
@@ -15,11 +15,17 @@ class ChartsView: UIView {
     var chartsData: ChartsResponseModel?
     
     override func draw(_ rect: CGRect) {
+        let rectForChartsNavigation = CGRect.init(x: 0, y: rect.height, width: rect.width, height: rect.height * 0.1)
+        drawChartsNavigation(in: rectForChartsNavigation)
+    }
+    
+    
+    private func drawChartsNavigation(in rect: CGRect) {
         if let chartData = chartsData?[selectedChartPosition] {
             let width = rect.width
             let height = rect.height
             var xData:[Int] = []
-            var chartsYData:[[Int]] = []
+            var chartsYData:[String:[Int]] = [:]
             chartData.columns.forEach { (column) in
                 if let nameElement = column.first, case let .string(name) = nameElement {
                     
@@ -35,35 +41,35 @@ class ChartsView: UIView {
                             if case let .integer(point) = yPoint {
                                 yData.append(point)
                             }
+                            
                         })
-                        chartsYData.append(yData)
+                        chartsYData[name] = yData
                     }
                     
                 }
             }
             
             var maxElements:[Int] = []
-            chartsYData.forEach({ maxElements.append($0.max() ?? 0) })
+            chartsYData.forEach({ maxElements.append($0.value.max() ?? 0) })
             
             let yHeightPointStep = height / CGFloat(integerLiteral: maxElements.max() ?? 0)
             let xWidthPointStep = width/CGFloat(integerLiteral: xData.count)
-            let path = UIBezierPath()
-            path.lineWidth = 1.0
             chartsYData.forEach { (yData) in
+                let path = UIBezierPath()
+                path.lineWidth = 1.0
                 xData.enumerated().forEach { (offset: Int, element: Int) in
-                    let xPos = CGFloat(offset) * xWidthPointStep - 20
-                    let yPos = height - (CGFloat(yData[offset]) * yHeightPointStep)
+                    let xPos = CGFloat(offset) * xWidthPointStep
+                    let yPos = rect.minY - (CGFloat(yData.value[offset]) * yHeightPointStep)
                     if offset == 0 {
                         path.move(to: .init(x: xPos, y: yPos))
                     } else {
                         path.addLine(to: .init(x: xPos, y: yPos))
                     }
                 }
+                UIColor.init(hexFromString: chartData.colors[yData.key] ?? "").setStroke()
+                path.stroke()
             }
-            
-            path.stroke()
             
         }
     }
-    
 }
